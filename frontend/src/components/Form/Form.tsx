acb02227fa92a2644f22f24ffac81690f1c406d5
bhumika.patil-coditas <bhumika.patil@coditas.com>
2026-07-13 16:03:26 +0530

    Reusable Button, Form, FormInput, Sidebar created. Added global styling

--- frontend/src/components/Form/Form.tsx
+++ frontend/src/components/Form/Form.tsx
@@ -1,7 +1,8 @@
 import type { FormProps } from "./Form.types";
+import styles from "./Form.module.scss";
 
-const Form = ({ children }: FormProps) => {
-    return <form>{children}</form>
+const Form = ({ children , ...props}: FormProps) => {
+    return <form className={styles.form} {...props}>{children}</form>
 }
 
 export default Form;
---
+++ frontend/src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import styles from "./Header.module.scss";
+
+const Header = () => {
+    return (
+        <header className={styles.header}>
+            <p>Admin</p>
+        </header>
+    )
+}
+export default Header;
