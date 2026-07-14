4ab02a7d92b0ac3979a5f7bfbfe320058345c3c3
deep-patil-coditas <deep.patil@coditas.com>
2026-07-14 19:01:27 +0530

    change Jwt algo and try authentication

--- backend/auth-service/src/main/java/com/example/auth_service/util/JwtUtil.java
+++ backend/auth-service/src/main/java/com/example/auth_service/util/JwtUtil.java
@@ -3,13 +3,16 @@ package com.example.auth_service.util;
 import com.example.auth_service.entities.User;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import java.util.Date;
+import java.util.Random;
 
 @Component
 public class JwtUtil {
@@ -17,22 +20,19 @@ public class JwtUtil {
     private final long expirationTime;
     private final SecretKey key;
 
-
     public JwtUtil(@Value("${jwt.secret}")String SECRET,
                    @Value("${jwt.expiration}")long expirationTime){
-
         this.key= Keys.hmacShaKeyFor(SECRET.getBytes());
         this.expirationTime=expirationTime;
-
     }
 
     public String generateToken(User user) {
         return Jwts.builder()
-                .subject(user.getEmail())
+                .setSubject(user.getEmail())
                 .claim("role", user.getRole())
-                .claim("username", user.getUsername())
-                .issuedAt(new Date())
-                .expiration(new Date(System.currentTimeMillis() + expirationTime))
+                .claim("name", user.getName())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expirationTime))
                 .signWith(key)
                 .compact();
     }
@@ -49,13 +49,31 @@ public class JwtUtil {
         return parseToken(token).getSubject();
     }
 
-    public boolean validateToken(User userDetails, String username, String token) {
-        return  username.equals(userDetails.getEmail()) && !isTokenExpired(token);
+    public boolean validateToken(User employee, String username, String token) {
+        return  username.equals(employee.getEmail()) && !isTokenExpired(token);
     }
 
+    public User getCurrentUser() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth == null || !auth.isAuthenticated()) {
+            throw new UsernameNotFoundException("Not authenticated");
+        }
+        return  (User) auth.getPrincipal();
+    }
+
+    public long getReamingExpirationTime(String token) {
+        Date expiry = getUsernameFromToken(token) != null
+                ? parseToken(token).getExpiration() : new Date();
+        long remainingSeconds = expiry.getTime() - System.currentTimeMillis();
+        return Math.max(0,remainingSeconds);
+
+    }
+
+    public String getOtp(){
+        return  (100000 + new Random().nextInt(900000))+"";
+    }
     private boolean isTokenExpired(String token) {
         return parseToken(token).getExpiration().before(new Date());
     }
 
 }
-
