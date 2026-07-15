9650d3145ae4b991914a68e9da4052b3e827cceb
yashdivekar-coditas <yash.divekar@coditas.com>
2026-07-15 14:27:22 +0530

    set up event module an dits core packages.

---
+++ backend/event-service/main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI, Depends
+import uvicorn
+from src.api import __all__
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app:FastAPI = FastAPI(title="Event service")
+
+@app.get("/ping")
+def health():
+    return "pong"
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+for router in __all__:
+    app.include_router(router)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app" , port=8002, reload=True)
